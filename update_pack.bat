ea15af80fa3c91f24e8f43d53244966f414916c6
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2024-07-07 15:34:25 +0200

    Update update_pack.bat

--- update_pack.bat
+++ update_pack.bat
@@ -1,3 +1,13 @@
+@echo off
+
+set folderName=BluedySymphony
+
 powershell -command "Start-BitsTransfer -Source https://github.com/BS-Sunaria/BluedySymphony/archive/refs/heads/main.zip -Destination folder.zip"
 powershell -command "Expand-Archive folder.zip -DestinationPath (Get-Location) -Force"
-powershell -command "Rename-Item -Path 'BluedySymphony-main' -NewName 'BluedySymphony'"
+powershell -command "if (Test-Path -Path '%folderName%' -PathType Container) { Remove-Item -Path '%folderName%' -Force -Recurse }"
+powershell -command "Rename-Item -Path 'BluedySymphony-main' -NewName '%folderName%' -Force"
+powershell -command "Remove-Item -Path folder.zip -Force"
+
+echo Operations completed, if you encountered an error, contact the operator with the error message.
+
+pause
